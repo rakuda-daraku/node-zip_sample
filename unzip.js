321d5f2069cbabebf0dce2068c50dae436554dbb
rakuda-daraku <rakugaki.oukoku@gmail.com>
2017-06-22 21:58:24 +0900

    不要なコード削除 コメント追加

--- unzip.js
+++ unzip.js
@@ -5,9 +5,9 @@ fs.readFile('test.zip', 'binary', function(err, data){
   var zip = new require('node-zip')(data, {base64: false, checkCRC32: true});
 
   for(var i in zip.files) {
-    // console.log(zip.files[i]);
     for(var j in zip.files[i]) {
       if (j == '_data') {
+        // 展開後のファイルのテキストの内容を表示
         console.log(zip.files[i][j]);
       }
     }
